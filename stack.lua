849a078c7a7adec030a075349f8477493237986a
Mermersk <ic4ruz39@gmail.com>
2018-08-08 01:39:45 +0200

    First upload only allowed one instance of the class to be created and all other that were created were really just pointers to the first time someone called stack:create(). THis has been fixed. Now the class is more in line with OOP(thanks to metatables) and multiple instances can be created

--- stack.lua
+++ stack.lua
@@ -1,27 +1,39 @@
 --A stack implementation
 --Stack table is the table contaning stack functions, s is the stack table
+
 stack = {}
-
-function stack:create()
-  s = {}
-  return stack --This is done so that the user can name their stack, f.ex: jojo = stack:create() and then he can perform function like jojo:push(123)
+stack_mt = {__index = stack} --Metatable to table s, its index points to the stack table
+function stack:create() --Functions inside of table stack
+  local s = {} --New object/table, is only accesible to 1 instance
+  setmetatable(s, stack_mt) --set stack_mt to be metatable of s
+  return s
 end
 
-function stack:push(obj)
-  table.insert(s, 1, obj)
+function stack:push(object) --The colon here is syntax sugar for the function below:
+  table.insert(self, 1, object) --The self here is refering to the current instance of the table s
 end
+
+--function stack.push(self, object)
+  --table.insert(self, 1, object)
+--end
+
+function stack:peek()
+  print(self[1]) --The self here is refering to the current instance of the table s
+  return self[1]
+end
+
 
 function stack:pop()
-  local obj = s[1]
-  table.remove(s, 1)
+  local obj = self[1]
+  table.remove(self, 1)
   return obj
 end
 
-function stack:peek()
-  print(s[1])
-  return s[1]
+function stack:size()
+  print(#self)
+  return #self
 end
 
-function stack:size()
-  print(#s)
+function stack:toTable()
+  return self
 end
